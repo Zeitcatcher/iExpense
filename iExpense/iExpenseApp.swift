a0caa4c2e4ad6868cbed77b7bc39da9790a5c6c8
Zeitcatcher <ars.oksenoyt@gmail.com>
2024-05-14 13:41:22 -1000

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Arseniy Oksenoyt on 5/14/24.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
