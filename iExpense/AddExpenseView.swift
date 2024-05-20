ece874e4b83dc0e868781727c448a52a93183837
Zeitcatcher <ars.oksenoyt@gmail.com>
2024-05-19 16:36:43 -1000

    Added editable navigationTitile

--- iExpense/AddExpenseView.swift
+++ iExpense/AddExpenseView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct AddView: View {
     @Environment(\.dismiss) var dismiss
     
-    @State private var name = ""
+    @State private var name = "New Expense"
     @State private var type = "Personal"
     @State private var amount = 0.0
     
@@ -20,27 +20,26 @@ struct AddView: View {
     let localCurrency = Locale.current.currency?.identifier ?? "USD"
     
     var body: some View {
-        VStack {
-            Form {
-                TextField("Name", text: $name)
-                
-                Picker("Type", selection: $type) {
-                    ForEach(types, id: \.self) {
-                        Text($0)
+        NavigationStack {
+            VStack {
+                Form {
+                    Picker("Type", selection: $type) {
+                        ForEach(types, id: \.self) {
+                            Text($0)
+                        }
                     }
+                    
+                    TextField("Amount", value: $amount, format: .currency(code: localCurrency))
+                        .keyboardType(.decimalPad)
                 }
-                
-                TextField("Amount", value: $amount, format: .currency(code: localCurrency))
-                    .keyboardType(.decimalPad)
+                .navigationTitle($name)
+                .navigationBarTitleDisplayMode(.inline)
             }
-            
-        }
-        .overlay(alignment: .bottom) {
-            Button("Cancerl") { 
-                dismiss()
+            .overlay(alignment: .bottom) {
+                Button("Cancerl") {
+                    dismiss()
+                }
             }
-//                .padding(.bottom)
-//                .frame(width: 150, height: 50)
         }
         
         .toolbar {
